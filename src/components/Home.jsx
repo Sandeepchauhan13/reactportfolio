d5bb73050d733e851942c41d67910f37ce6416f8
apnainstitute-demo <sinh.pozta1@gmail.com>
2023-12-15 19:26:45 +0530

    update size and and add in para

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,10 +13,11 @@ const Home = () => {
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold my-3 text-white">
+          <h2 className="text-4xl sm:text-5xl font-bold my-3 text-white">
             I'm a Full Stack Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
+            Full Stack developer
           I have a five years of Diploma and Degree in IT education. 
           I want to learn all my life and i have passion for coding. I have worked as a free lancer for four years with big tech gaints.
           I love to work with React, javascript , html, css, tailwindcss, Next Js, TypeScript , Redux , Node , MongoDb, and express.
